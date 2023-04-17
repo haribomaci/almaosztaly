9d62f2eec75cd66dbbfdfc568b851b744f9685a4
toth julia <t.julia982@gmail.com>
2023-04-17 14:42:49 +0200

    kész dolgozat

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+* File: App.js
+* Author: Tóth Júlia
+* Copyright: 2023, Tóth Júlia
+* Date: 2023-04-17
+* Group: Szoft 2 N
+* Github: https://github.com/haribomaci/
+* Licenc: GNU GPL
+*
+*/
 
-export default function App() {
+
+import React, { useState } from 'react';
+import {StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const App = () => {
+  const [firstClass, setFirstClass] = useState('');
+  const [secondClass, setSecondClass] = useState('');
+  const [total, setTotal] = useState(0);
+  const [firstClassPercent, setFirstClassPercent] = useState(0);
+  const [secondClassPercent, setSecondClassPercent] = useState(0);
+
+  const calculatePercentages = () => {
+    const firstClassNum = parseInt(firstClass);
+    const secondClassNum = parseInt(secondClass);
+    const totalNum = firstClassNum + secondClassNum;
+    const firstClassPercentNum = (firstClassNum / totalNum) * 100;
+    const secondClassPercentNum = (secondClassNum / totalNum) * 100;
+    setTotal(totalNum);
+    setFirstClassPercent(firstClassPercentNum);
+    setSecondClassPercent(secondClassPercentNum);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.about}>Tóth Júlia, 2023-04-17, szoft 2/n</Text>
+      <Text style={styles.title}>Almák számlálása</Text>
+      <Text style={styles.text}>Első osztályú almák: </Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        onChangeText={setFirstClass}
+        value={firstClass}
+      />
+      <Text style={styles.text}>Másod osztályú almák: </Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        onChangeText={setSecondClass}
+        value={secondClass}
+      />
+      <Button style={styles.button} title="Számítás" onPress={calculatePercentages} />
+      <Text>Összes alma: {total}</Text>
+      <Text>Első osztályú almák: {firstClassPercent.toFixed(2)}%</Text>
+      <Text>Másod osztályú almák: {secondClassPercent.toFixed(2)}%</Text>
+
       <StatusBar style="auto" />
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +66,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  about: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    backgroundColor: 'yellow'
+  },
+  button: {
+    color: 'green'
+  },
 });
+export default App;
